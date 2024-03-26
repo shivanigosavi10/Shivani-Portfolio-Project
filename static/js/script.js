0db13a46669d7f15a0d68ceafb41280641540143
shivanigosavi10 <shivanigosavi943@gmail.com>
2024-03-26 22:08:20 +0530

    initial commit

---
+++ static/js/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky")
+        }else{
+            $('.navbar').removeClass("sticky")
+        }
+        if(this.scroll > 500){
+            $('.scroll-up-btn').addClass("show")
+        }
+        else{
+            $('.scroll-up-btn').removeClass("show")
+        }
+    });
+
+    //slide-up script
+    /*('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop:2});
+    });*/
+
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i.active').toggleClass("active");
+    });
+});
+
+ //slide-up script
+    ('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop:0});
+    });
